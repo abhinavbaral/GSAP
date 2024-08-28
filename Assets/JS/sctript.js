1b40f88b8196c19f1f45f54832705ec512306814
Abhinav Baral <abhinavbaral@gmail.com>
2024-08-28 17:32:28 +0545

    scrolltrigger

--- Assets/JS/sctript.js
+++ Assets/JS/sctript.js
@@ -22,18 +22,50 @@
 //   yoyo: true,
 // });
 
+// navbar items
 let tl = gsap.timeline();
 
 tl.from("h2", {
-    y: -20,
-    opacity: 0,
-    duration: 1,
-    delay: 0.5
+  y: -20,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
 });
 
 tl.from("h4", {
-    y: -20,
-    opacity: 0,
-    duration: 1,
-    stagger: 0.5 
-}); 
+  y: -20,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.5,
+});
+
+gsap.from("#PG1 #box", {
+  scale: 0,
+  duration: 2,
+  delay: 1,
+  rotate: 360
+});
+
+gsap.from("#PG2 #box", {
+  scale: 0,
+  duration: 2,
+  delay: 1,
+  rotate: 360,
+  scrollTrigger:{
+    trigger:"#PG2 #box",
+    scroller: "body",
+    markers:true
+  }
+});
+
+gsap.from("#PG3 #box", {
+  scale: 0,
+  duration: 2,
+  delay: 1,
+  rotate: 360,
+  scrollTrigger:{
+    trigger:"#PG3 #box",
+    scroller: "body",
+    markers:true
+  }
+});
